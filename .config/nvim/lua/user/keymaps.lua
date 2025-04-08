870b40f8d89b8691a3acf00dcc055fbb35defd16
Wayne Robison <reallyblackbuffalo@gmail.com>
2025-04-07 17:33:19 -0700

    Switch to using `<Cmd>` in mappings where possible.
    This allowed me to combine the normal and insert mode mappings for
    moving lines up and down. Some mappings in visual mode, etc., need to
    continue using a regular `:` because they rely on the visual mode marks
    getting added automatically to the beginning of the command as the range
    to operate on, or they're more complicated commands including embedded
    `<CR>`s that seem to mess up the parsing for `<Cmd>`, so I left them
    as-is.
    I also added a mapping of Space to no-op so that if I press space as my
    leader and then don't press anything else quickly enough it won't
    advance me a character.
    There was also one mapping where I'd accidentally left off the closing
    `>` on the end of the mapping, so I fixed that so that the mapping
    works. Tested all the mappings to make sure everything's working as
    expected.

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,24 +1,25 @@
 -- Set leader and local leader to space
+vim.keymap.set("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Toggle Show Whitespace
-vim.keymap.set("n", "<Leader>ts", ":set list! list?<CR>")
+vim.keymap.set("n", "<Leader>ts", "<Cmd>set list! list?<CR>")
 
 -- Toggle Tabs
-vim.keymap.set("n", "<Leader>tt", ":set expandtab! expandtab?<CR>")
+vim.keymap.set("n", "<Leader>tt", "<Cmd>set expandtab! expandtab?<CR>")
 
 -- Toggle explorer
-vim.keymap.set("n", "<Leader>e", ":Lexplore 30<CR>")
+vim.keymap.set("n", "<Leader>e", "<Cmd>Lexplore 30<CR>")
 
 -- Press Space twice to turn off highlighting and clear any message already displayed.
-vim.keymap.set("n", "<Leader><Leader>", ":nohlsearch<Bar>:echo<CR>", { silent = true })
+vim.keymap.set("n", "<Leader><Leader>", "<Cmd>nohlsearch<CR><Cmd>echo<CR>", { silent = true })
 
 -- Toggle Highlighting, and show current value.
-vim.keymap.set("n", "<Leader>th", ":set hlsearch! hlsearch?<CR>")
+vim.keymap.set("n", "<Leader>th", "<Cmd>set hlsearch! hlsearch?<CR>")
 
 -- Toggle Ignorecase
-vim.keymap.set("n", "<Leader>ti", ":set ignorecase! ignorecase?<CR")
+vim.keymap.set("n", "<Leader>ti", "<Cmd>set ignorecase! ignorecase?<CR>")
 
 -- Highlight all occurrences of the current word.
 vim.keymap.set("n", "<Leader>H", ":let @/='\\<<C-R>=expand(\"<cword>\")<CR>\\>'<CR>:set hls<CR>")
@@ -30,16 +31,14 @@ vim.keymap.set("n", "<C-K>", "<C-W>k")
 vim.keymap.set("n", "<C-L>", "<C-W>l")
 
 -- Move current line/selection up/down using Alt-k/j
-vim.keymap.set("n", "<A-k>", ":m -2<CR>")
-vim.keymap.set("n", "<A-j>", ":m +<CR>")
-vim.keymap.set("i", "<A-k>", "<ESC>:m -2<CR>gi")
-vim.keymap.set("i", "<A-j>", "<ESC>:m +<CR>gi")
+vim.keymap.set({ "n", "i" }, "<A-k>", "<Cmd>m -2<CR>")
+vim.keymap.set({ "n", "i" }, "<A-j>", "<Cmd>m +<CR>")
 vim.keymap.set("x", "<A-k>", ":m '<-2<CR>gv")
 vim.keymap.set("x", "<A-j>", ":m '>+<CR>gv")
 
 -- Mappings for sourcing the current file or running the current or highlighted Lua lines.
 -- These are originally from the suggestions in TJ DeVries' video:
 -- [Everything You Need To Start Writing Lua](https://youtu.be/CuWfgiwI73Q?si=9VGWKluwNvZtkfvl)
-vim.keymap.set("n", "<Leader>sf", "<cmd>source %<CR>")
-vim.keymap.set("n", "<Leader>x", ":.lua<CR>")
+vim.keymap.set("n", "<Leader>sf", "<Cmd>source %<CR>")
+vim.keymap.set("n", "<Leader>x", "<Cmd>.lua<CR>")
 vim.keymap.set("v", "<Leader>x", ":lua<CR>")
