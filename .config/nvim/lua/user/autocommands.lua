f06aac4e80fc31f7e6bdfa0c9bbeddb23405cb56
Wayne Robison <reallyblackbuffalo@gmail.com>
2025-04-07 18:29:15 -0700

    Add some autocommands for filetype-specific mappings to Neovim config.
    These are from the "Learn Vimscript the Hard Way" book. I originally
    added these on the other branch for doing the exercises from the book,
    and translated them into Lua for my Neovim config.
    There's a bunch of other settings and mappings and autocommands and
    things from the book, but these seem to be the most useful for right
    now. I can always add more of them later if I want to.
    - Comment out lines in different languages
    - Fold the current tag in html file

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -14,3 +14,42 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		end
 	end,
 })
+
+-- Helper function for setting buffer-local keymaps
+local local_keymap = function(mode, lhs, rhs)
+	vim.keymap.set(mode, lhs, rhs, { buffer = true })
+end
+
+-- Autocommands to set mappings to comment out the current line in different languages.
+-- Might end up moving these to files in after/ftplugin.
+local comment_keymap = function(comment_chars)
+	local lhs = "<LocalLeader>c"
+	local normal_rhs = "I" .. comment_chars .. "<esc>"
+	local_keymap("n", lhs, normal_rhs)
+	local_keymap("v", lhs, ":normal " .. normal_rhs)
+end
+local comment_mappings_group = vim.api.nvim_create_augroup("filetype_comment_mappings", { clear = true })
+local register_comment_keymap = function(filetypes, comment_chars)
+	vim.api.nvim_create_autocmd("Filetype", {
+		pattern = filetypes,
+		desc = "Set local keymaps for commenting lines in " .. table.concat(filetypes, ", ") .. " files.",
+		group = comment_mappings_group,
+		callback = function ()
+			comment_keymap(comment_chars)
+		end,
+	})
+end
+register_comment_keymap({ "javascript", "c", "cpp", "java" }, "//")
+register_comment_keymap({ "python" }, "#")
+register_comment_keymap({ "vim" }, '"')
+register_comment_keymap({ "lua" }, "--")
+
+-- Autocommand to set mapping for folding the current tag in an html file.
+vim.api.nvim_create_autocmd("Filetype", {
+	pattern = { "html" },
+	desc = "Set local keymap for folding the current tag in html files.",
+	group = vim.api.nvim_create_augroup("filetype_html", { clear = true }),
+	callback = function()
+		local_keymap("n", "<LocalLeader>f", "Vatzf")
+	end,
+})
