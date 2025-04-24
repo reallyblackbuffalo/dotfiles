f62ad49f7fbd467f4b4e88543fa959e1046b4d50
Wayne Robison <reallyblackbuffalo@gmail.com>
2025-04-24 15:38:27 -0700

    Clean things up a bit from the last commit.
    In my vscode mappings I figured out that I don't have to check myself for
    if the folder I'm opening is already open in a VSCode window, so that
    allowed me to simplify things quite a bit. I also did some renaming, made
    the helper functions local, and created a local variable to store the
    imported vscode module. I also switched the `open_folder` helper function
    to use the same pattern as the other one where it will return a function
    that can be passed directly to `vim.keymap.set` to simplify things.

--- .config/nvim/lua/user/vscode-keymaps.lua
+++ .config/nvim/lua/user/vscode-keymaps.lua
@@ -1,16 +1,18 @@
 -- VSCode-specific customizations to keymaps
 
+local vscode = require('vscode')
+
 -- Helper function to make it easier to call VSCode actions from keymaps.
 -- Returns a function that can be passed to the keymap that when called executes the given action in VSCode.
-callVscodeActionHelper = function(action)
-    return function() require('vscode').action(action) end
+local vscode_action_rhs = function(action)
+    return function() vscode.action(action) end
 end
 
 -- Toggle Show Whitespace
 -- The list option from Neovim isn't currently synced with VSCode's corresponding
 -- setting in the vscode-neovim extension, so I need to remap it here to toggle
 -- the right setting for me.
-vim.keymap.set("n", "<Leader>ts", callVscodeActionHelper('editor.action.toggleRenderWhitespace'))
+vim.keymap.set("n", "<Leader>ts", vscode_action_rhs('editor.action.toggleRenderWhitespace'))
 
 -- Easier window navigation
 -- Need to remap these with recursive mapping enabled so that the special mappings
@@ -21,51 +23,42 @@ vim.keymap.set("n", "<C-K>", "<C-W>k", { remap = true })
 vim.keymap.set("n", "<C-L>", "<C-W>l", { remap = true })
 
 -- Toggle the sidebar
-vim.keymap.set("n", "<Leader>b", callVscodeActionHelper('workbench.action.toggleSidebarVisibility'))
+vim.keymap.set("n", "<Leader>b", vscode_action_rhs('workbench.action.toggleSidebarVisibility'))
 
 -- Toggle file explorer focus
 -- Use VSCode's builtin file explorer rather than trying to use netrw, which opens
 -- in a separate tab due to it being a new buffer.
-vim.keymap.set("n", "<Leader>E", callVscodeActionHelper('workbench.view.explorer'))
+vim.keymap.set("n", "<Leader>E", vscode_action_rhs('workbench.view.explorer'))
 
 -- Toggle search in sidebar
-vim.keymap.set("n", "<Leader>F", callVscodeActionHelper('workbench.view.search'))
+vim.keymap.set("n", "<Leader>F", vscode_action_rhs('workbench.view.search'))
 
 -- Toggle Git/Source Control in sidebar
-vim.keymap.set("n", "<Leader>g", callVscodeActionHelper('workbench.view.scm'))
+vim.keymap.set("n", "<Leader>g", vscode_action_rhs('workbench.view.scm'))
 
 -- Find Files (like telescope, but using VSCode's quick open file picker)
-vim.keymap.set("n", "<Leader>ff", callVscodeActionHelper('workbench.action.quickOpen'))
+vim.keymap.set("n", "<Leader>ff", vscode_action_rhs('workbench.action.quickOpen'))
 
 -- Open the Command Palette
-vim.keymap.set("n", "<Leader>p", callVscodeActionHelper('workbench.action.showCommands'))
+vim.keymap.set("n", "<Leader>p", vscode_action_rhs('workbench.action.showCommands'))
 
 -- Toggle the panel (for the terminal, etc)
-vim.keymap.set("n", "<Leader>j", callVscodeActionHelper('workbench.action.togglePanel'))
+vim.keymap.set("n", "<Leader>j", vscode_action_rhs('workbench.action.togglePanel'))
 
 -- Open keyboard shortcuts
-vim.keymap.set("n", "<Leader>ks", callVscodeActionHelper('workbench.action.openGlobalKeybindings'))
+vim.keymap.set("n", "<Leader>ks", vscode_action_rhs('workbench.action.openGlobalKeybindings'))
 
--- Helper function for opening the given folder in a new window unless it is already open in the current window.
-open_folder = function(folderToOpen)
-    local vscode = require('vscode')
-    local already_open = vscode.eval([[
-        const fs = require("fs");
-        const pathToOpen = fs.realpathSync(args.folderToOpen);
-        const currentPath = fs.realpathSync(vscode.workspace.workspaceFolders[0].uri.fsPath);
-        return pathToOpen === currentPath;
-    ]], { args = { folderToOpen = folderToOpen }})
-
-    if not already_open then
-        local uri = vscode.eval('return vscode.Uri.file(args.path)', { args = { path = folderToOpen } })
+-- Helper function for opening the given folder in a new window.
+-- Returns a function that can be passed to a keymap that when called opens the given folder in a new VSCode window.
+local open_folder_rhs = function(folder)
+    return function()
+        local uri = vscode.eval('return vscode.Uri.file(args.path)', { args = { path = folder} })
         vscode.action('vscode.openFolder', { args = { uri, { forceNewWindow = true }}})
-    else
-        print(foldertoopen .. "is already open")
     end
 end
 
 -- Open Neovim config
-vim.keymap.set("n", "<Leader>en", function() open_folder(vim.fn.stdpath('config')) end)
+vim.keymap.set("n", "<Leader>en", open_folder_rhs(vim.fn.stdpath('config')))
 
 -- Open dotfiles config
-vim.keymap.set("n", "<Leader>ed", function() open_folder(vim.fn.expand('$HOME') .. '/.dotfiles') end)
+vim.keymap.set("n", "<Leader>ed", open_folder_rhs(vim.fn.expand('$HOME') .. '/.dotfiles'))
