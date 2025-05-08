411747e7da0eb1d8099015faa5c84a87c11bc0eb
Wayne Robison <reallyblackbuffalo@gmail.com>
2025-05-08 15:03:28 -0700

    Update modes for a bunch of keymaps across Vim, Neovim, and VsVim.
    I realized that a bunch of the keymaps in my vimrc and vsvimrc were using just the standard `noremap`,
    which applies to normal, visual, and operator-pending modes, and that doesn't necessarily make sense for
    a lot of the mappings. Some work best only in normal mode and some are okay in normal and visual mode,
    but none of them really make much sense in operator-pending mode. In fixing this I also realized that
    in Neovim I had these same mappings just using normal mode for the most part, and so I also expanded
    the mappings to visual mode there. It's a lot cleaner using the Lua API in Neovim since you can map the
    desired set of modes explicitly, whereas in Vim if you want a combination of modes that isn't already
    represented by a specific mapping command, then you have to duplicate the lines for those mappings for
    each mode you want them in.

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,5 +1,5 @@
 -- Set leader and local leader to space (when updating, be sure to also update vscode_leader_key below)
-vim.keymap.set("", "<Space>", "<Nop>")
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -9,22 +9,22 @@ if vim.g.vscode then
 end
 
 -- Toggle Show Whitespace
-vim.keymap.set("n", "<Leader>ts", "<Cmd>set list! list?<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>ts", "<Cmd>set list! list?<CR>")
 
 -- Toggle Tabs
-vim.keymap.set("n", "<Leader>tt", "<Cmd>set expandtab! expandtab?<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>tt", "<Cmd>set expandtab! expandtab?<CR>")
 
 -- Toggle file explorer
 vim.keymap.set("n", "<Leader>E", "<Cmd>Lexplore 30<CR>")
 
 -- Press Space twice to turn off highlighting and clear any message already displayed.
-vim.keymap.set("n", "<Leader><Leader>", "<Cmd>nohlsearch<CR><Cmd>echo<CR>", { silent = true })
+vim.keymap.set({ "n", "v" }, "<Leader><Leader>", "<Cmd>nohlsearch<Bar>echo<CR>", { silent = true })
 
 -- Toggle Highlighting, and show current value.
-vim.keymap.set("n", "<Leader>th", "<Cmd>set hlsearch! hlsearch?<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>th", "<Cmd>set hlsearch! hlsearch?<CR>")
 
 -- Toggle Ignorecase
-vim.keymap.set("n", "<Leader>ti", "<Cmd>set ignorecase! ignorecase?<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>ti", "<Cmd>set ignorecase! ignorecase?<CR>")
 
 -- Highlight all occurrences of the current word.
 vim.keymap.set("n", "<Leader>H", ":let @/='\\<<C-R>=expand(\"<cword>\")<CR>\\>'<CR>:set hls<CR>")
